35fe72fe82319e4e33704dd05e082cf233ccb9a3
Bősz Norbert <norbert.bosz@nitrowise.com>
2022-05-09 11:52:36 +0200

    Add Moshi

--- app/src/main/java/hu/bme/aut/pokedb/di/NetworkModule.kt
+++ app/src/main/java/hu/bme/aut/pokedb/di/NetworkModule.kt
@@ -1,15 +1,25 @@
 package hu.bme.aut.pokedb.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import hu.bme.aut.pokedb.network.PokemonController
+import retrofit2.Converter
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    fun provideMoshi(): Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides
+    fun provideConverterFactory(moshi: Moshi): Converter.Factory = MoshiConverterFactory.create(moshi)
 
     @Provides
     fun provideRetrofit(): Retrofit {
