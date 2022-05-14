8e728ec05182cefedfaee45b9b38adeae27d404c
Norbert Attila Bősz <norbert.bosz@nitrowise.com>
2022-05-14 18:29:11 +0200

    Google Analytics and Crashlytics

--- app/src/main/java/hu/bme/aut/pokedb/ui/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/pokedb/ui/MainActivity.kt
@@ -2,14 +2,27 @@ package hu.bme.aut.pokedb.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import hu.bme.aut.pokedb.R
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firebaseAnalytics = Firebase.analytics
+
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN) {
+            param(FirebaseAnalytics.Param.START_DATE, Calendar.getInstance().getTime().toString())
+        }
     }
 }
